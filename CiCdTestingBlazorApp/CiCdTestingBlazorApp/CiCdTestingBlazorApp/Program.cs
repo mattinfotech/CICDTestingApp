9e1e204ca0a995b498cfed53128bfbf81770ed7c
Matt Lagat <matias.lagat@techfabric.com>
2025-04-20 13:27:59 +0800

    Initialize repository

---
+++ CiCdTestingBlazorApp/CiCdTestingBlazorApp/CiCdTestingBlazorApp/Program.cs
@@ -0,0 +1,35 @@
+using CiCdTestingBlazorApp.Client.Pages;
+using Microsoft.FluentUI.AspNetCore.Components;
+using CiCdTestingBlazorApp.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveWebAssemblyComponents();
+builder.Services.AddFluentUIComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(CiCdTestingBlazorApp.Client._Imports).Assembly);
+
+app.Run();
